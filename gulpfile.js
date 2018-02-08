b8daf7edd0ac4ae2ed148675deee96a3201fcca2
Alexandre AUDEON <alexandre.audeon@niji.fr>
2018-02-08 16:45:44 +0100

    Restructuration du theme

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var compress = 'expanded'; // expanded, nested, compact, compressed
 
 gulp.task('sass', function(){
-   gulp.src('src/scss/**/*.scss')
+   gulp.src('scss/**/*.scss')
        .pipe(sass({outputStyle: compress}).on('error', sass.logError))
        .pipe(concat('atomic_theme.css'))
        .pipe(autoprefixer())
-       .pipe(gulp.dest('css'))
+       .pipe(gulp.dest('public/css'))
 });
 
 gulp.task('scripts', function(){
-    gulp.src('src/js/**/*.js')
+    gulp.src('scripts/**/*.js')
         .pipe(concat('atomic_theme.js'))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('public/js'))
 });
 
 gulp.task('watch', function(){
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['scripts']);
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('scripts/**/*.js', ['scripts']);
 });
